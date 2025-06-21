0cd4d53b33bc4b76b69571289fac3e76ec9201bc
Kevin Curruchich <kevinc@mayfer.dev>
2025-06-21 16:41:47 -0600

    fix: update nuxt.config.ts for Firebase hosting preset and adjust package.json build script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
+  nitro: {
+    preset: "firebase-app-hosting",
+    firebase: {
+      gen: 2,
+    },
+  },
   devtools: { enabled: true },
   routeRules: {
     "/": { prerender: true },
