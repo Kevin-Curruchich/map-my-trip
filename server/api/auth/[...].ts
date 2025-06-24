17fbdd31f9e0f24f8e6c28591d9eacdbcc29f529
Kevin Curruchich <kevinc@mayfer.dev>
2025-06-24 12:50:21 -0600

    fix: update auth handler to improve error handling and clean up code; add source map upload configuration in wrangler.toml

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -3,35 +3,44 @@ import Google from "next-auth/providers/google";
 import { createServerUserService } from "~/server/utils/userService";
 
 export default NuxtAuthHandler({
+  debug: true,
   secret: process.env.NUXT_SECRET,
   providers: [
-    // @ts-expect-error Use .default here for it to work during SSR.
-    Google.default({
+    Google({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
   callbacks: {
     jwt({ token, account, profile }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account && profile) {
-        token.sessionToken = account.access_token;
-        token.profile = profile;
-        token.sub = profile.sub || ""; // Use sub as the unique identifier
+      try {
+        // Persist the OAuth access_token to the token right after signin
+        if (account && profile) {
+          token.sessionToken = account.access_token;
+          token.profile = profile;
+          token.sub = profile.sub || ""; // Use sub as the unique identifier
+        }
+        return token;
+      } catch (error) {
+        console.error("JWT callback error:", error);
+        throw new Error("Failed to create JWT token");
       }
-      return token;
     },
     async session({ session, token }) {
-      if (token.sub) {
-        const userService = createServerUserService();
-        const user = await userService.findByExternalId(token.sub);
-
-        if (user) {
-          session.user.id = user.id; // Database ID
+      try {
+        if (token.sub) {
+          const userService = createServerUserService();
+          const user = await userService.findByExternalId(token.sub);
+          if (user) {
+            session.user.id = user.id; // Database ID
+          }
+          session.user.sub = token.sub; // Add sub to session user
         }
-        session.user.sub = token.sub; // Add sub to session user
+        return session;
+      } catch (error) {
+        console.error("Session callback error:", error);
+        throw new Error("Failed to retrieve session");
       }
-      return session;
     },
     async signIn({ user, account, profile }) {
       if (account?.provider === "google" && profile) {
